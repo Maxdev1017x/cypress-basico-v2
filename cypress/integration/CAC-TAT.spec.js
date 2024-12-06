f7b756c53dd914988ac9b10f772deb8f177dfc5f
Maxdev1017x <Thelastmc1@gmail.com>
2024-12-06 18:10:46 -0300

    test: Adicionar teste para requisição HTTP e ajustes no arquivo de testes
    - Adicionado teste que verifica a resposta de uma requisição HTTP para o endpoint especificado.
    - Verificada a desestruturação da resposta para capturar status, statusText e ody.
    - Assegurado que o status da resposta é 200, o statusText é 'OK', e o body contém 'CAC TAT'.
    - Removido o only do arquivo privacyjs.

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -198,12 +198,23 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
 
-    it.only('Preenche a área de texto usando o comando "invoke"', function(){
+    it('Preenche a área de texto usando o comando "invoke"', function(){
         const longText = Cypress._.repeat('0123456789', 20)
 
         cy.get('#open-text-area')
         .invoke('val', longText)
         .should('have.value', longText)
+    })
+
+
+    it('Faz uma requisição HTTP', function(){
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+        .should(function(response){
+        const {status, statusText, body} = response
+        expect(status).to.equal(200)
+        expect(statusText).to.equal('OK')
+        expect(body).to.include('CAC TAT')
+        })
     })
 });
 
